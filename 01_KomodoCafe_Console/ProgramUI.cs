bf7e84394c4841abfd912324b714cd575c982925
Russell Rockwood <russell.rockwood@gmail.com>
2021-05-17 22:29:54 -0400

    add new menu UI

--- 01_KomodoCafe_Console/ProgramUI.cs
+++ 01_KomodoCafe_Console/ProgramUI.cs
@@ -13,6 +13,8 @@ namespace _01_KomodoCafe_Console
         public void Run()
         {
             SeedMenuList();
+            Menu();
+
         }
 
         public void SeedMenuList()
@@ -26,6 +28,98 @@ namespace _01_KomodoCafe_Console
             _repo.AddNewMenuItem(menuItem2);
             _repo.AddNewMenuItem(menuItem3);
             _repo.AddNewMenuItem(menuItem4);
+        }
+
+        public void Menu()
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("Komodo Cafe Crud\n");
+                Console.WriteLine("Select a menu option:\n" +
+                    "1. Create New Menu Item\n" +
+                    "2. View All Menu Items\n" +
+                    "3. Update existing Item\n" +
+                    "4. Delete Item\n" +
+                    "5. Exit App");
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        AddItem();
+                        break;
+                    case "2":
+                        // DisplayMenu();
+                        break;
+                    case "3":
+                        // UpdateItem();
+                        break;
+                    case "4":
+                        // DeleteItem();
+                        break;
+                    case "5":
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+                }
+            }
+        }
+
+
+        public void AddItem()
+        {
+            Console.Clear();
+            KomodoCafeItem newItem = new KomodoCafeItem();
+
+            //Console.WriteLine("Enter the MealNumber\n" +
+            //    "");
+            //string mealNumberString = Console.ReadLine();
+            //newItem.MealNumber = Convert.Double(mealNumberString); 
+
+            Console.WriteLine("Enter Mealname:");
+            newItem.MealName = Console.ReadLine();
+
+            Console.WriteLine("Enter Description:");
+            newItem.Description = Console.ReadLine();
+
+            List<string> newIngredientList = new List<string>();
+            bool addMoreIngredients = true;
+            while (addMoreIngredients)
+            {
+                
+                Console.WriteLine("Enter Ingredient:\n" +
+                    "Enter \"Done\" when finished");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "done")
+                {
+                    addMoreIngredients = false;
+                }
+                else
+                {
+                    newIngredientList.Add(userInput);
+                    Console.WriteLine("Ingredient Added");
+                }
+                
+            }
+
+            Console.WriteLine("Enter Price:");
+            newItem.Price = Convert.ToDouble(Console.ReadLine());
+
+            bool wasAddedCorrectly = _repo.AddNewMenuItem(newItem);
+            if (wasAddedCorrectly)
+            {
+                Console.WriteLine("New Menu Item Succesfully Added");
+            }
+            else
+            {
+                Console.WriteLine("Error adding menu item");
+            }
         }
 
     }
